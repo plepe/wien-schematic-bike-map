964fcd545bdf0c461fbdf5d5db0a392d8fa71e64
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-06-15 02:52:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<svg width="1000" height="1000" style='overflow: auto'>
+<circle cx="500" cy="500" r="200" stroke-width="3" stroke="red" fill="none" />
+</svg>
+</body>
+</html>
